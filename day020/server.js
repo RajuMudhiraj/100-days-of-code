328ed8eeacdeb16d5ce41a11c678b3619e8573ca
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-01-01 21:28:16 +0530

    created a basic express.js server and added a basic route

--- day020/server.js
+++ day020/server.js
@@ -1,34 +1,14 @@
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 
-var corsOptions = {
-    origin: 'http://localhost:8081'
-};
-
-app.use(cors(corsOptions));
-
-const db = require("./app/models");
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
-// sync({force:true}).then(()=>{
-// console.log("Drop and re-sync db.")
-// });
-
-// parse requests of content-type-application/json
-app.use(express.json());
-// parse requests of content-type-application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get('/', (req, res) => {
-    res.json({ message: "Welcome to postgresql application." });
+    res.json({ message: "Welcome to Node.js and Express.js" });
 });
 
-require("./app/routes/turorial.routes")(app);
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
