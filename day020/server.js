a72da6b7ab27c364dda557eb1244168ec8d0dcff
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-01-01 22:06:03 +0530

    Installed body-parser library and added middleware to parse the json and url encoded requests

--- day020/server.js
+++ day020/server.js
@@ -1,10 +1,16 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 
 // addingg cors middleware to enable cross origin resource sharing
 app.use(cors())
+
+// adding body parser middleware to parse json and url encoded requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 // simple route
