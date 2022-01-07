bd4790795a9f7573f93dd5281c349560efe9247f
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-01-07 20:53:59 +0530

    Deleted previous code

--- day026_Postgresql_Express/db.js
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'movies',
-    password:'Raz2485',
-    port:5432
-});
-module.exports = pool;
