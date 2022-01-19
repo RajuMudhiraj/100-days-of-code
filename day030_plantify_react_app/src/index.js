27bb6fd11a1b6199b8c7a406d4ac589e02924452
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-01-19 14:42:24 +0530

    Added route

--- day030_plantify_react_app/src/index.js
+++ day030_plantify_react_app/src/index.js
@@ -1,15 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import LandingPage from './pages/LandingPage/LandingPage';
+import Logo from './components/Logo/Logo';
+import Header from './layouts/Header/Header';
+import Body from './layouts/Body/Body';
+import Footer from './layouts/Footer/Footer';
+import Button from './components/Button/Button';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage className="landingPage"/>}>
+          <Route index element={<Button className="plant1TreeBtn" value="Plant1Tree" />} />
+          <Route path="signIn" element={<Button className='signInBtn' value="Sign In" />} />
+          <Route path="signUp" element={<Button className='signUpBtn' value="Sign Up" />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- day030_plantify_react_app/src/pages/LandingPage/LandingPage.js
+++ day030_plantify_react_app/src/pages/LandingPage/LandingPage.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import Header from '../../layouts/Header/Header'
-import Body from '../../layouts/Body/Body'
-import Footer from '../../layouts/Footer/Footer'
+import { Outlet, Link } from "react-router-dom";
+import Logo from "../../components/Logo/Logo";
+import Button from "../../components/Button/Button";
+import Footer from "../../layouts/Footer/Footer";
 import './LandingPage.css'
 
-
-export default class LandingPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-  }
-
-  render() {
-    return (
-      <div className='landingPage'>
-        <Header className='header' />
-        <Body />
-        <Footer />
-
+const LandingPage = () => {
+  return (
+    <>
+      < div className="landingPage">
+        <div>
+          <Link to="/"><Logo /></Link>
+        </div>
+        <div className="landingPage">
+          <Link to="/signIn"><Button className="signInBtn" value="Sign In" /></Link>
+          <Link to="/signUp"><Button className="signUpBtn" value="Sign Up" /></Link>
+        </div>
       </div>
-    );
-  }
-}
+      <div className="outLet">
+        <Outlet />
+      </div>
+      <Footer />
+    </>
+  )
+};
+
+export default LandingPage;
+
