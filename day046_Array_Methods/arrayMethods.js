39caf8912a8b996f5ce978740eda3e18923ba3b6
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-02-03 22:41:47 +0530

    Revisited some array methods

---
+++ day046_Array_Methods/arrayMethods.js
@@ -0,0 +1,103 @@
+
+// construct from elements
+const arrayFromElements = new Array("ele1", "ele2", "ele3")
+console.log(arrayFromElements)
+
+// construct from array length
+const arrayFromLength = new Array(10)
+console.log(arrayFromLength)
+
+// ----------Array.fill()-------------------
+const array1 = [1, 2, 3, 4];
+
+// fill with 0 from position 2 until position 4
+console.log(array1.fill(0, 2, 4));
+// expected output: [1, 2, 0, 0]
+
+// fill with 5 from position 1
+console.log(array1.fill(5, 1));
+// expected output: [1, 5, 5, 5]
+
+console.log(array1.fill(6));
+// expected output: [6, 6, 6, 6]
+
+// -------- Array.concat() -------------------
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+console.log(array3);
+// expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+// --------------------- Array.copyWithin()--------------------------
+const array1 = ['a', 'b', 'c', 'd', 'e'];
+
+// copy to index 0 the element at index 3
+console.log(array1.copyWithin(0, 3, 4));
+// expected output: Array ["d", "b", "c", "d", "e"]
+
+// copy to index 1 all elements from index 3 to the end
+console.log(array1.copyWithin(1, 3));
+// expected output: Array ["d", "d", "e", "d", "e"]
+
+// ------------ Array.entries()--------------------------
+const array2 = ['a', 'b', 'c'];
+
+const iterator1 = array1.entries();
+
+console.log(iterator1.next().value);
+// expected output: Array [0, "a"]
+
+console.log(iterator1.next().value);
+// expected output: Array [1, "b"]
+
+// ----------------- Array.every()---------------
+function isSubset(array1, array2) {
+    // returns true if array2 is a subset of array1
+
+    return array2.every(function (element) {
+        return array1.includes(element);
+    });
+}
+
+console.log(isSubset([1, 2, 3, 4, 5, 6, 7], [5, 7, 6])); // true
+console.log(isSubset([1, 2, 3, 4, 5, 6, 7], [5, 8, 7])); // false
+
+// ------------ Array.fill()--------------------------
+const array1 = [1, 2, 3, 4];
+
+// fill with 0 from position 2 until position 4
+console.log(array1.fill(0, 2, 4));
+// expected output: [1, 2, 0, 0]
+
+// fill with 5 from position 1
+console.log(array1.fill(5, 1));
+// expected output: [1, 5, 5, 5]
+
+console.log(array1.fill(6));
+// expected output: [6, 6, 6, 6]
+
+
+// ------------------- Array.filter()--------------
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
+
+// ---------------- Array.find()---------------------
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find(element => element == 12);
+
+console.log(found);
+// expected output: 12
+
+// --------------------- Array.findIndex()--------------------
+const array1 = [5, 12, 8, 130, 44];
+
+const isLargeNumber = (element) => element > 13;
+
+console.log(array1.findIndex(isLargeNumber));
+// expected output: 3
